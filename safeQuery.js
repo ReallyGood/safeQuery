2b85f2f0f793de53795e9b87c275bb33bc3145b3
Ronny Orbach <r@ronnyo.com>
2012-07-08 10:13:00 +0300

    backup all props on jQuery

--- safeQuery.js
+++ safeQuery.js
@@ -1,11 +1,23 @@
 (function($){
 if(typeof $ === 'undefined') return;
 
-window.$$ = function(){
-	var selection=jQuery.apply(this,arguments);
+var _$ = $,
+	props = {};
+
+$.each($, function(i, prop){
+	props[i] = prop;
+});
+
+window.$ = function(){
+	var selection = _$.apply(this,arguments);
 	selection.length==0 && console && console.warn && console.warn(selection.selector+':jQuery selector returned no matches');
 	return selection;
 };
 
+$.each(props, function(i, prop){
+	window.$[i] = props[i];
+});
+
+window.jQuery = $;
 
 })(jQuery);
